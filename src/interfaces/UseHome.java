c761c04089f48491fec0edabee2bedd53ceca81b
dineshelan <dineshelan99@gmail.com>
2024-02-14 10:00:42 +0530

    Changes in OOPS3

--- src/interfaces/UseHome.java
+++ src/interfaces/UseHome.java
@@ -3,12 +3,12 @@ package interfaces;
 public class UseHome {
 	public static void main(String[] args) {
 		Home h = new Home();
-		System.out.println(h.chair("Tablemate"));
+		System.out.println(h.chair("Plastic Chair"));
 	    h.table(4);
 	    System.out.println(h.door());
 	    
 	    Furniture f = new Home();
-	    System.out.println(f.chair("Wooden Table"));
+	    System.out.println(f.chair("Tablemate"));
 	    f.table(5);
 	    
 	}
